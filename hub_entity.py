0a8c0e8cb6fe6af23dc5b1b09a52287ea1d619d3
TaihuLight <cloudiot@126.com>
2024-10-09 17:40:12 +0800

    init entity

--- hub_entity.py
+++ hub_entity.py
@@ -1,6 +1,7 @@
 
 from neomodel import StructuredNode, FloatProperty, IntegerProperty, DateTimeProperty, StringProperty, ArrayProperty, RelationshipTo, RelationshipFrom, config
-import json, os
+import json, os, ast
+from datetime import datetime
 
 def setup_neo4j(database_name = 'neo4j'):
      config.DATABASE_URL = 'bolt://neo4j:19841124@103.6.49.76:7687'
@@ -41,7 +42,7 @@ class Electrolyte(ChemNode):
 
 class Precursor(ChemNode):
      chem_function = StringProperty()
-     
+
      catelyst = RelationshipFrom("Catelyst", "React_with")
      electrolyte = RelationshipFrom("Electrolyte", "Needed_BY")
 
@@ -134,7 +135,7 @@ def generate_json_template(node_class):
           elif isinstance(property_obj, FloatProperty):
                properties[attr] = "Float-point value"
           elif isinstance(property_obj, ArrayProperty):
-               properties[attr] = "Array of strings such as ['string1', 'string2']"
+               properties[attr] = "['string1', 'string2']"
           elif isinstance(property_obj, DateTimeProperty):
                properties[attr] = "A datatime string such as '2023-10-04 00:00:00'"
 
@@ -151,7 +152,7 @@ def generate_json_template(node_class):
      # Create the JSON template
      json_template = {
           node_class.__name__: {
-               "label": node_class.__label__,
+               # "label": node_class.__label__,
                "properties": properties,
                "relationships": relationships
           }
@@ -161,13 +162,12 @@ def generate_json_template(node_class):
      return json.dumps(json_template, indent=4, ensure_ascii=False)
 
 
-if __name__ == '__main__':
-     node_clsname = ["Electrolyte", "Precursor", "Catelyst", "Conductiviy", "CrystalStructure", "SpaceGroup", "ReactionDevice", "ReactionCondition", "SynthesisOperation", "Reference"]
-     dir_path = os.path.dirname(os.path.realpath(__file__))
-     save_path = os.path.join(dir_path, 'json_template')
+def export_json_template(save_path):
      if not os.path.exists(save_path):
           os.makedirs(save_path)
-          
+     
+     node_clsname = ["Electrolyte", "Precursor", "Catelyst", "Conductiviy", "CrystalStructure", "SpaceGroup", "ReactionDevice", "ReactionCondition", "SynthesisOperation", "Reference"]
+     
      for clsname in node_clsname:
           node_class = globals()[clsname]
           # Generate and print the JSON template for the Person class
@@ -178,3 +178,71 @@ if __name__ == '__main__':
                json_file.write(json_template)
 
 
+def load_json_data(json_file):
+     with open(json_file) as pr_file:          
+          try:
+               pr_data = json.load(pr_file)
+          except Exception as e:
+               print(e)
+     return pr_data
+
+
+# Initialize a node with its properties and relationships using a JSON data
+def init_node(json_file):
+     pr_data = load_json_data(json_file)
+
+     try:
+          for cls_key in pr_data.keys():          
+               node_class = globals()[cls_key]
+               pr_value = pr_data[cls_key]
+               for pr_key in pr_value.keys():
+                    if pr_key == "properties":
+                         for prop_key in pr_value[pr_key].keys():
+                              prop_value = pr_value[pr_key][prop_key]
+                              ent_property = eval('node_class.%s' % prop_key)
+                              if isinstance(ent_property, StringProperty):
+                                   ent_property = prop_value
+                              elif isinstance(ent_property, IntegerProperty):
+                                   ent_property = int(prop_value)
+                              elif isinstance(ent_property, FloatProperty):
+                                   ent_property = float(prop_value)
+                              elif isinstance(ent_property, ArrayProperty):
+                                   ent_property = ast.literal_eval(prop_value)
+                              elif isinstance(ent_property, DateTimeProperty):
+                                   ent_property = datetime.strptime(prop_value, '%Y-%m-%d %H:%M:%S')
+                    
+     except Exception as e:
+          print(e)
+     
+     node_class.save()
+
+
+def update_relationships(json_file):
+     pr_data = load_json_data(json_file)
+                       
+     try:
+          for cls_key in pr_data.keys():          
+               node_class = globals()[cls_key]
+               pr_value = pr_data[cls_key]
+               for pr_key in pr_value.keys():
+                    if pr_key == "relationships":
+                         for rel_key in pr_value[pr_key].keys():
+                              relation_value = pr_value[pr_key][rel_key]
+                              ents_relation = eval('node_class.%s' % rel_key)
+                              if isinstance(ents_relation, RelationshipTo):
+                                   ents_relation = RelationshipTo("RelatedNode", "related_to")
+                              elif isinstance(ents_relation, RelationshipFrom):
+                                   ents_relation = RelationshipFrom("RelatedNode", "related_form")
+     except Exception as e:
+          print(e)
+     
+     node_class.save()
+
+
+if __name__ == '__main__':
+     save_dir='json_template'
+     save_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), save_dir)
+     # export_json_template(save_path)
+
+     json_file = os.path.join(save_path, 'Electrolyte_template.json')
+     init_node(json_file)
