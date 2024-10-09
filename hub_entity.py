6e8cf4387b3ad6b6432093668906af19bd74e06d
TaihuLight <cloudiot@126.com>
2024-10-09 12:49:14 +0800

    Export JSON template

--- hub_entity.py
+++ hub_entity.py
@@ -1,6 +1,6 @@
 
 from neomodel import StructuredNode, FloatProperty, IntegerProperty, DateTimeProperty, StringProperty, ArrayProperty, RelationshipTo, RelationshipFrom, config
-
+import json, os
 
 def setup_neo4j(database_name = 'neo4j'):
      config.DATABASE_URL = 'bolt://neo4j:19841124@103.6.49.76:7687'
@@ -13,7 +13,7 @@ class ChemNode(StructuredNode):
      uid = StringProperty(unique_index=True, required=True)
      name = StringProperty(required=True)
      alias = ArrayProperty(base_property=StringProperty())
-     chem_formula = StringProperty()
+     chem_formula = StringProperty(help_text="The chemical formula of the entity.")
 
 
 # Experiment Node such as device, operations, etc
@@ -112,3 +112,66 @@ class Reference(StructuredNode):
     published_name = StringProperty()   # Journal/Conference name
     published_date = DateTimeProperty()   # Date of publication
     
+
+
+# Export the node definitions to a JSON file for collecting its properties and relationships, without sorting alphabetically
+def generate_json_template(node_class):
+     properties = {}
+     relationships = {}
+
+     for att_array in node_class.__all_properties__:          
+     # for attr in dir(node_class): # Properties and relationships are sorted alphabetically
+          # if not attr.startswith('_'):     # Skip private attributes and methods
+               # property_obj = getattr(node_class, attr)  # Get the attribute's type
+          attr = att_array[0]
+          property_obj = getattr(node_class, attr)
+          if isinstance(property_obj, StringProperty):
+               properties[attr] = "String"
+          elif isinstance(property_obj, IntegerProperty):
+               properties[attr] = "Integer"
+          elif isinstance(property_obj, FloatProperty):
+               properties[attr] = "Float-point value"
+          elif isinstance(property_obj, ArrayProperty):
+               properties[attr] = "Array of strings"
+          elif isinstance(property_obj, DateTimeProperty):
+               properties[attr] = "A datatime string such as '2023-10-04 00:00:00'"
+
+     for att_array in node_class.__all_relationships__: 
+          attr = att_array[0] 
+          property_obj = getattr(node_class, attr)    
+          # Check for relationships
+          if isinstance(property_obj, RelationshipTo):
+               relationships[attr] = "related_to: A array of json files"  # The related node class 
+          elif isinstance(property_obj, RelationshipFrom):
+               relationships[attr] = "related_form: A array of json files"
+
+
+     # Create the JSON template
+     json_template = {
+          node_class.__name__: {
+               "properties": properties,
+               "relationships": relationships
+          }
+     }
+
+     # Convert to JSON
+     return json.dumps(json_template, indent=4, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+     node_clsname = ["Electrolyte", "Precursor", "Catelyst", "Conductiviy", "CrystalStructure", "SpaceGroup", "ReactionDevice", "ReactionCondition", "SynthesisOperation", "Reference"]
+     dir_path = os.path.dirname(os.path.realpath(__file__))
+     save_path = os.path.join(dir_path, 'json_template')
+     if not os.path.exists(save_path):
+          os.makedirs(save_path)
+          
+     for clsname in node_clsname:
+          node_class = globals()[clsname]
+          # Generate and print the JSON template for the Person class
+          json_template = generate_json_template(node_class)
+          print(json_template)
+          # Write to a file
+          with open(os.path.join(save_path, f'{clsname}_template.json'), 'w', encoding='utf-8') as json_file:
+               json_file.write(json_template)
+
+
